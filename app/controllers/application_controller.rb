8c9af31564a2f6b4156817280e9bb44c5e00ce01
John Storm <jcstorm@cps.edu>
2014-07-23 17:57:39 +0000

    added index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,10 @@ class ApplicationController < ActionController::Base
     u.name = params['User Name']
     u.image_url = params['Image URL']
     u.description = params['Bio']
-    u.save
+    if u.save
     redirect_to "/users/#{u.id}" #ruby string interpolation is required here to access the u.id variable
+    else render 'new'
+    end
   end
   
   def edit
@@ -29,14 +31,21 @@ class ApplicationController < ActionController::Base
     u.name = params['user']
     u.image_url = params['image']
     u.description = params['bio']
-    u.save
+    if u.save
     redirect_to "/users/#{u.id}" #ruby string interpolation is required here to access the u.id variable
+    else
+      redirect_to 'users/#{u.id}/update'
+    end
   end
   
   def destroy
     u = User.find_by_id(params['id'])
     u.destroy
-    redirect_to "/users/new_user"
+    redirect_to "/users"
+  end
+  
+  def index
+    @users = User.all
   end
 
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
 
+  #create
   get '/users/new_user' => 'application#new'
   get '/users/create_user' => 'application#create'
+  
+  #read
   get '/users/:id'=> 'application#show'
+  get '/users' => 'application#index'
+  
+  #update
   get '/users/:id/edit' => 'application#edit'
-  get '/users/:id/destroy' => 'application#destroy'
   get '/users/:id/update'=> 'application#update'
+  
+  #destroy
+ get '/users/:id/destroy' => 'application#destroy'
+
   
 end
